5371933682620194a36ad933c6de777d829a2dd7
F0x <Operator-F0x@proton.me>
2024-01-17 10:23:32 +0100

    Add files via upload

--- Cyber-Breakout/lib0/windows.py
+++ Cyber-Breakout/lib0/windows.py
@@ -4,15 +4,25 @@ from pygame.locals import *
 pygame.init() #initializes pygame window
 pygame.display.set_caption('KeyCast') #titlebar caption
 
-GOSURF=pygame.display.set_mode((1800,1000),0,32) #sets main surface
+GOSURF=pygame.display.set_mode((600,400),0,32) #sets main surface
 
 gobackground = pygame.image.load('Python\\Cyber-Breakout\\img\\gameover.jpg') #background image for game
 
-"""--------------------------------------------------------------------------"""
-while True:
 
-    def writetext():
+pygame.init()
+screen = pygame.display.set_mode((1280, 720))
+clock = pygame.time.Clock()
 
+
+"""--------------------------------------------------------------------------
+le chiamate alle funzioni sono di prova e il while 1 potra essere messo in main.py con al interno le chiamate
+
+"""
+
+
+while 1:
+
+#funzione per la chiusura del gioco
     def quitgame():
         """exits programme without any errors"""
         for event in pygame.event.get(): #quitting process
@@ -21,6 +31,7 @@ while True:
                 sys.exit() #system quit
     quitgame()
 
+#funzione per posizione un immagine di background
     def Surface():
         GOSURF.blit(gobackground,(0,0)) #background image
 
